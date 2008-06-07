5395156f4663ea3d78302bcbb458231ecdb5e53c
zziemke <zziemke@d547e7e5-d63b-0410-bf60-a3c7e541264b>
2008-06-07 12:05:05 +0000

    - upgrade to 2.0 TODO: - Fix 16:9 Resolution Support - make textures static - store cues inside of Audio.cs

--- trunk/XNA Extreme 202 - Precision/Classes/Config.cs
+++ trunk/XNA Extreme 202 - Precision/Classes/Config.cs
@@ -27,49 +27,49 @@ namespace Precision.Classes
         internal static readonly Color timePowerupPickupTimeBarColor = Color.White;
         internal const float TIME_POWERUP_SLOWDOWN = 0.1f;
 
-        internal const float EXTRALIFE_POWERUP_RESPAWN_TIME = 3f; // 3
-        internal const float EXTRALIFE_POWERUP_FREQUENCY = 0.6f;
+        internal const float EXTRALIFE_POWERUP_RESPAWN_TIME = 0.2f; // 3
+        internal const float EXTRALIFE_POWERUP_FREQUENCY = 1f;
         internal const float EXTRALIFE_POWERUP_PICKUP_TIME = 5f;
-        internal const int EXTRALIFE_POWERUP_START_LEVEL = 7; //7
+        internal const int EXTRALIFE_POWERUP_START_LEVEL = 1; //7
         internal static readonly Color extralifePowerupPickupTimeBarColor = Color.White;
 
-        internal const float SCALE_DOWN_POWERUP_RESPAWN_TIME = 2f; // 2
-        internal const float SCALE_DOWN_POWERUP_FREQUENCY = 0.4f;
+        internal const float SCALE_DOWN_POWERUP_RESPAWN_TIME = 0.2f; // 2
+        internal const float SCALE_DOWN_POWERUP_FREQUENCY = 1f;//0.4
         internal const float SCALE_DOWN_POWERUP_PICKUP_TIME = 5f;
-        internal const int SCALE_DOWN_POWERUP_START_LEVEL = 4; //4
+        internal const int SCALE_DOWN_POWERUP_START_LEVEL = 1; //4
         internal static readonly Color scaleDownPowerupPickupTimeBarColor = Color.White;
         internal const float SCALE_DOWN_POWERUP_SCALE = 0.5f;
         internal const float SCALE_DOWN_POWERUP_SCALE_TIME = 2.5f;
 
         internal const float SCORE_POWERUP_DURATION = 4f;
-        internal const float SCORE_POWERUP_RESPAWN_TIME = 1f;
-        internal const float SCORE_POWERUP_FREQUENCY = 0.5f;
+        internal const float SCORE_POWERUP_RESPAWN_TIME = 0.2f;
+        internal const float SCORE_POWERUP_FREQUENCY = 1f;
         internal const float SCORE_POWERUP_PICKUP_TIME = 4f;
-        internal const int SCORE_POWERUP_START_LEVEL = 3; //3
+        internal const int SCORE_POWERUP_START_LEVEL = 1; //3
         internal static readonly Color scorePowerupActiveTimeBarColor = Color.Orange;
         internal static readonly Color scorePowerupPickupTimeBarColor = Color.White;
 
         internal const float SHIELD_POWERUP_DURATION = 5f;
-        internal const float SHIELD_POWERUP_RESPAWN_TIME = 5f;
-        internal const float SHIELD_POWERUP_FREQUENCY = 0.5f;
+        internal const float SHIELD_POWERUP_RESPAWN_TIME = 0.2f;
+        internal const float SHIELD_POWERUP_FREQUENCY = 1f;
         internal const float SHIELD_POWERUP_PICKUP_TIME = 5f;
-        internal const int SHIELD_POWERUP_START_LEVEL = 8; //5
+        internal const int SHIELD_POWERUP_START_LEVEL = 1; //5
         internal static readonly Color shieldPowerupActiveTimeBarColor = Color.LightBlue;
         internal static readonly Color shieldPowerupPickupTimeBarColor = Color.White;
 
         internal const float DESTRUCTION_POWERUP_DURATION = 5f;
-        internal const float DESTRUCTION_POWERUP_RESPAWN_TIME = 4f;
-        internal const float DESTRUCTION_POWERUP_FREQUENCY = 0.4f;//0.4
+        internal const float DESTRUCTION_POWERUP_RESPAWN_TIME = 0.2f;
+        internal const float DESTRUCTION_POWERUP_FREQUENCY = 1f;//0.4
         internal const float DESTRUCTION_POWERUP_PICKUP_TIME = 6f;
-        internal const int DESTRUCTION_POWERUP_START_LEVEL = 9;//9
+        internal const int DESTRUCTION_POWERUP_START_LEVEL = 1;//9
         internal static readonly Color destructionPowerupActiveTimeBarColor = Color.Red;
         internal static readonly Color destructionPowerupPickupTimeBarColor = Color.White;
         
         internal const float SPEED_POWERDOWN_DURATION = 10f;
-        internal const float SPEED_POWERDOWN_RESPAWN_TIME = 5f;
-        internal const float SPEED_POWERDOWN_FREQUENCY = 0.8f;
+        internal const float SPEED_POWERDOWN_RESPAWN_TIME = 0.2f;
+        internal const float SPEED_POWERDOWN_FREQUENCY = 1f;
         internal const float SPEED_POWERDOWN_PICKUP_TIME = CELL_DEATH_TIME_GENERIC;
-        internal const int SPEED_POWERDOWN_START_LEVEL = 2; //2
+        internal const int SPEED_POWERDOWN_START_LEVEL = 1; //2
         internal static readonly Color speedPowerdownActiveTimeBarColor = Color.Red;
         internal static readonly List<Color> speedPowerdownPickupTimeBarColors;
         internal const float SPEED_POWERDOWN_SLOWDOWN = 0.4f;
